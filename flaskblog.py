1ec2ff79a93b08cc63a47994207cba1dd2545154
rahulmistry33 <priyarahul0307@gmail.com>
2019-06-12 15:40:45 +0530

    added templates statics

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
+posts=[
+    {
+        'Author':'Rahul Mistry',
+        'Title':'Blog Post 1',
+        'Content':'Some shitty quotes',
+        'Date':'April 21'
+    },
+    {
+        'Author':'Gayatri Srinivasan',
+        'Title':'Blog Post 2',
+        'Content':'Some shitty quotes',
+        'Date':'Feb 1'
+    }
+]
 #@app.route("/")
 @app.route("/home")
-def hello():
-    return "<h1>Hello World!</h1>"
+def callHome():
+    return render_template("home.html", posts=posts)
 @app.route("/About")
 def callAbout():
-    return "<h1>About page</h1>"
+    return render_template("about.html",title="about")
+    
 
 if __name__=="__main__":
     app.run(debug=True)
