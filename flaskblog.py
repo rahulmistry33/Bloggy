969b1524770bd3fb5bec30b22a1f22ce8b42f5fb
rahulmistry33 <priya0307@gmail.com>
2019-06-15 15:06:03 +0530

    added routes and login

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect
 #this import for login and register from forms.py
 from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
@@ -30,17 +30,32 @@ def callAbout():
     return render_template("about.html",title="about")
 
 #a route for register
-@app.route("/register")
+@app.route("/register",methods=['GET','POST'])
 def callRegister():
     #creating instance of RegistrationForm class .. object is form
     form=RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}','succcess')
+        #redirecting after validating form 
+        return redirect(url_for('callHome'))
+
+
     return render_template('register.html',title='Register',form=form)
 
 #a route for login
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def callLogin():
     #creating instance of LoginForm class .. object is form
     form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data=="rahul.mistry@somaiya.edu" and form.password.data=="123":
+            flash('you have logged in successfully!','succcess')
+            return redirect(url_for('callHome'))
+        else:
+            flash('you have entered wrong email or password','danger')
+
+        
+       
     return render_template('login.html',title='Login',form=form)
     
 
