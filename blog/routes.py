66a094df9d9addf8731b6e99ca76d1eb9b17b9e1
rahulmistry33 <priyarahul0307@gmail.com>
2019-06-25 16:58:01 +0530

    added account info

--- blog/routes.py
+++ blog/routes.py
@@ -77,6 +77,7 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-    return render_template("account.html",title="account")
+    image_file=url_for('static',filename='profile_pics/'+ current_user.image_file)
+    return render_template("account.html",title="account",image_file=image_file)
 
     
